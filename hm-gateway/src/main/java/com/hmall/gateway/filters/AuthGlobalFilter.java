ae983877c09b6b5a751da082af89efa6b2e8ca1b
Zhou Jialong <1258681956@qq.com>
2026-01-30 11:13:56 +0800

    配置网关、登录校验以及网关传递用户信息到微服务

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
@@ -0,0 +1,70 @@
+package com.hmall.gateway.filters;
+
+import com.hmall.common.exception.UnauthorizedException;
+import com.hmall.gateway.config.AuthProperties;
+import com.hmall.gateway.util.JwtTool;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.security.auth.login.LoginException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@EnableConfigurationProperties(AuthProperties.class)
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
+
+    private final AuthProperties authProperties;
+    private final JwtTool jwtTool;
+    private final AntPathMatcher antPathMatcher=new AntPathMatcher();
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        if(isExclude(request.getPath().toString())){
+            return chain.filter(exchange);
+        }
+        List<String> headers = request.getHeaders().get("authorization");
+        String token=null;
+        if(headers!=null && !headers.isEmpty()){
+            token = headers.get(0);
+        }
+        Long userId=null;
+        try{
+            userId = jwtTool.parseToken(token);
+        }catch (UnauthorizedException e){
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        //保存信息到请求头中
+            //对传递到下游的信息做修改
+        String userInfo = userId.toString();
+        ServerWebExchange serverWebExchange = exchange.mutate()
+                .request(builder -> builder.header("user-info", userInfo))
+                .build();
+        return chain.filter(serverWebExchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+    private boolean isExclude(String path) {
+        for (String excludePath : authProperties.getExcludePaths()) {
+            if(antPathMatcher.match(excludePath,path)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- user-server/src/main/java/com/hmall/user/UserApplication.java
+++ user-server/src/main/java/com/hmall/user/UserApplication.java
@@ -3,7 +3,6 @@ package com.hmall.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("com.hmall.user.mapper")
 @SpringBootApplication
